9af77cebdd350ceae68b0529cb4c958aebc99486
awiede <awiede.dev@gmail.com>
2019-02-24 19:32:11 -0500

    call WMATA station info API

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"github.com/awiede/where-is-wmata/wmata"
+	"log"
+)
+
+func main() {
+
+	wmataKey := flag.String("wmata_key", "", "API key used to access WMATA API")
+	metroLine := flag.String("metro_line", "", "The metro line to get information for")
+
+	flag.Parse()
+
+	if *wmataKey == "" {
+		log.Fatalf("flag: wmata_key is required")
+	}
+
+	wmataService := wmata.NewService(*wmataKey)
+
+	stations, err := wmataService.GetStationsByLine(*metroLine)
+
+	if err != nil {
+		log.Fatalf("unable to retrieve station information, got error: %s", err)
+	}
+
+	log.Println(stations)
+
+}
