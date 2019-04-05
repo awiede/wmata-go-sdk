9d19f4a298c4ba7a3c2295380fa3e8905df27f1f
awiede <awiede.dev@gmail.com>
2019-04-04 22:08:13 -0400

    consolidate util/client code. Remove main.go and manual test config

--- wmata/client.go
+++ wmata/client.go
@@ -3,9 +3,29 @@ package wmata
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
+
+const (
+	LineCodeAll    = ""
+	LineCodeBlue   = "BL"
+	LineCodeGreen  = "GR"
+	LineCodeOrange = "OR"
+	LineCodeRed    = "RD"
+	LineCodeSilver = "SV"
+	LineCodeYellow = "YL"
+
+	APIKeyHeader = "api_key"
+)
+
+// CloseResponseBody is a helper function to close response body and log error
+func CloseResponseBody(response *http.Response) {
+	if closeErr := response.Body.Close(); closeErr != nil {
+		log.Printf("error closing response body: %s", closeErr)
+	}
+}
 
 type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
